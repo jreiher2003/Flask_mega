d6cf42c4eeea8a4f6805b915966fc9c5df624601
Jeff Reiher <jeffreiher@gmail.com>
2015-06-22 19:54:34 -0400

    fix typo

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ OPENID_PROVIDERS = [
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLACHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLACHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
--- db_create.py
+++ db_create.py
@@ -6,7 +6,7 @@ from app import db
 import os.path
 
 db.create_all()
-if not os.path.exixts(SQLALCHEMY_MIGRATE_REPO):
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
 	api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
 	api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
