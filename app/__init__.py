0fe83b16921c6e333b7ae6f834422b42911a511b
Jeff Reiher <jeffreiher@gmail.com>
2015-06-28 19:07:30 -0400

    add flask user_login
    Does actually work yet but it is getting there you have to signup
    for an Oauth provider and get a session token to plug in so kind of
    stupid but it has all the other code needed.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
+import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy 
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+# note ID is cap
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+lm.login_message = 'Yo needs to log back in son'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
